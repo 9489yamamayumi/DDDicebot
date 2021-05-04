9f814c87b520388c162faf69f7bbec3199f84007
9489yamamayumi <64786748+9489yamamayumi@users.noreply.github.com>
2021-05-04 15:25:26 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -25,18 +25,18 @@ async def echo(ctx,arg):
 
 @bot.command()
 async def r(ctx,arg):
-#    random.seed()
-#    vals=arg.split('d')
-#    times=int(vals[0])
-#    sides=int(vals[1])
-#    results=[]
-#    rsltsum=0
-#    for i in range(times):{
-#        results.append(random.randint(1,sides))
-#        rsltsum+=results[i]
-#    }
-#    await ctx.send(f"{arg} >> {rsltsum} : {results}")
-     await ctx.send(f"ok")
+    random.seed()
+    vals=arg.split('d')
+    times=int(vals[0])
+    sides=int(vals[1])
+    results=[]
+    rsltsum=0
+    for i in range(times):{
+        results.append(random.randint(1,sides))
+        rsltsum+=results[i]
+    }
+    await ctx.send(f"{arg} >> {rsltsum} : {results}")
+    await ctx.send(f"ok")
 
 
 bot.run(token)
